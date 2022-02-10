cce0493904588625904804cd5b7f9e06371e084e
nordy86 <83031494+nordy86@users.noreply.github.com>
2022-02-10 21:30:51 +0500

    Hw7-DONE

--- src/test/java/lesson6/PageObjectSiteHw6AndHw7Test.java
+++ src/test/java/lesson6/PageObjectSiteHw6AndHw7Test.java
@@ -3,17 +3,18 @@ package lesson6;
 import com.geekbrains.Lesson_6.LoginPage;
 import com.geekbrains.Lesson_6.MainPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import org.junit.jupiter.api.Test;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
+@Story("Проверка авторизации")
 public class PageObjectSite_hw6_Test {
     WebDriver driver;
     private final static String TheSite_URL = "http://practice.automationtesting.in/";
@@ -30,6 +31,7 @@ public class PageObjectSite_hw6_Test {
     }
 
     @Test
+    @Description("Проверка авторизации ранее зарегистрированного пользователя")
     void theSiteAuthTest() throws InterruptedException {
 //        new LoginPage(driver).loginInput.sendKeys("radugalocus@gmail.com");
 //        new LoginPage(driver).passwordInput.sendKeys("098123Qq_!$");
@@ -37,7 +39,8 @@ public class PageObjectSite_hw6_Test {
                 .clickMyAccButton()
                 .fillLogin("radugalocus@gmail.com")
                 .fillPassword("098123Qq_!$")
-                .loginButtonClick();
+                .loginButtonClick()
+                .checkLogin();
 
 
     }
